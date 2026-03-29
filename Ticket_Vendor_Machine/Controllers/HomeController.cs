d48e56a02644718766e1c6c121d4d2865e2dfc1a
thinh <sutianer0000@gmail.com>
2026-03-29 16:55:19 +0700

    updated calculate fare display

--- Ticket_Vendor_Machine/Controllers/HomeController.cs
+++ Ticket_Vendor_Machine/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ticket_Vendor_Machine.ServiceFunction;
 
 namespace Ticket_Vendor_Machine.Controllers
 {
@@ -11,6 +12,15 @@ namespace Ticket_Vendor_Machine.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        private readonly FareCalculator _fareCalc = new FareCalculator();
+
+        [HttpGet]
+        public JsonResult CalculateFare(int destinationId)
+        {
+            decimal fare = _fareCalc.Calculate(destinationId);
+            return Json(new { fare = fare }, JsonRequestBehavior.AllowGet);
         }
 
     }
---
+++ Ticket_Vendor_Machine/ServiceFunction/FareCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ticket_Vendor_Machine.ServiceFunction
+{
+    public class FareCalculator
+    {
+        
+        private const int BaseStationId = 1;   
+        private const decimal BaseFare = 7000; 
+        private const decimal FarePerStop = 5000;
+        private const decimal MaxFare = 30000; 
+
+        public decimal Calculate(int destinationId)
+        {
+            int stops = destinationId - BaseStationId; 
+            if (stops <= 0) return 0;
+
+            decimal fare = BaseFare + (stops - 1) * FarePerStop;
+            return Math.Min(fare, MaxFare);
+        }
+    }
+}
